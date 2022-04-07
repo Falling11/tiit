785f793396df2f7391b805600d9a9ba51ba2a8ed
ivanboika <deadman2002xxx@gmail.com>
2022-04-08 01:44:53 +0300

    Task is done

--- test_3333/TeamMembers/Player.h
+++ test_3333/TeamMembers/Player.h
@@ -16,20 +16,14 @@ public:
 	Player(const Player&);
 	explicit Player( Player&&);
 
-	uint16_t tryTakeBall(const Player&);
-	uint16_t tryToGoal();
-	uint16_t moveToBall(const std::vector<Player*>&anotherTeam);
+	uint16_t tryTakeBall();
+	const uint16_t& getChanceToGoal() { return _possibiltyToGoal; }
 private:
 	/*
 	* @brief This struct includes coordinates of player
 	*/
 
-	uint16_t _xPos;
-	uint16_t _yPos;
-	
 	bool _hasBall;
-	bool _hasTeamBall;
-
 	uint16_t _speed;
 	uint16_t _possibiltyToGoal;
 	uint16_t _chanceWinBall;
